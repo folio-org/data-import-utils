4ec52362e7856c85f47ea1486d377cbc7339cba8
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-03-11 17:41:02 +0200

    Add GenericHandlerAnswer class

---
+++ src/test/java/org/folio/dataimport/util/test/StubObject.java
@@ -0,0 +1,14 @@
+package org.folio.dataimport.util.test;
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Stub object for testing needs
+ */
+class StubObject {
+
+  void targetMethod(Future<JsonObject> future) { // NOSONAR
+    // no implementation
+  }
+}
