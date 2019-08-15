1717087a561cfed3268e21bd63940655b3596001
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-08-15 11:03:46 +0300

    Updated RMB version to 26.4.0

--- src/main/java/org/folio/dataimport/util/DaoUtil.java
+++ src/main/java/org/folio/dataimport/util/DaoUtil.java
@@ -4,7 +4,7 @@ import org.folio.rest.persist.Criteria.Criteria;
 import org.folio.rest.persist.Criteria.Limit;
 import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.cql.CQLWrapper;
-import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+import org.folio.cql2pgjson.CQL2PgJSON;
 
 public final class DaoUtil {
 
@@ -17,10 +17,10 @@ public final class DaoUtil {
    * @param query - query from URL
    * @param limit - limit of results for pagination
    * @return - CQL wrapper for building postgres request to database
-   * @throws org.z3950.zing.cql.cql2pgjson.FieldException field exception
+   * @throws org.folio.cql2pgjson.exception.FieldException field exception
    */
   public static CQLWrapper getCQLWrapper(String tableName, String query, int limit, int offset)
-    throws org.z3950.zing.cql.cql2pgjson.FieldException {
+    throws org.folio.cql2pgjson.exception.FieldException {
     return getCQLWrapper(tableName, query)
       .setLimit(new Limit(limit))
       .setOffset(new Offset(offset));
@@ -31,10 +31,10 @@ public final class DaoUtil {
    *
    * @param query - query from URL
    * @return - CQL wrapper for building postgres request to database
-   * @throws org.z3950.zing.cql.cql2pgjson.FieldException field exception
+   * @throws org.folio.cql2pgjson.exception.FieldException field exception
    */
   public static CQLWrapper getCQLWrapper(String tableName, String query)
-    throws org.z3950.zing.cql.cql2pgjson.FieldException {
+    throws org.folio.cql2pgjson.exception.FieldException {
     CQL2PgJSON cql2pgJson = new CQL2PgJSON(tableName + ".jsonb");
     return new CQLWrapper(cql2pgJson, query);
   }
@@ -50,7 +50,7 @@ public final class DaoUtil {
     Criteria criteria = new Criteria();
     criteria.addField(jsonbField);
     criteria.setOperation("=");
-    criteria.setValue(value);
+    criteria.setVal(value);
     return criteria;
   }
 }
