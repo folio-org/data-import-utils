9cde05e8eb9443933d4bd4b741b9178de5418037
Ruslan Lavrov <ruslan_lavrov@epam.com>
2019-10-08 12:21:55 +0300

    Updated rmb version

--- src/main/java/org/folio/dataimport/util/ExceptionHelper.java
+++ src/main/java/org/folio/dataimport/util/ExceptionHelper.java
@@ -3,7 +3,6 @@ package org.folio.dataimport.util;
 import io.vertx.core.Future;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.apache.http.HttpStatus;
 import org.folio.dataimport.util.exception.ConflictException;
 import org.folio.rest.tools.utils.ValidationHelper;
 
@@ -11,6 +10,11 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.CONFLICT;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 public final class ExceptionHelper {
 
@@ -21,19 +25,19 @@ public final class ExceptionHelper {
 
   public static Response mapExceptionToResponse(Throwable throwable) {
     if (throwable instanceof BadRequestException) {
-      return Response.status(HttpStatus.SC_BAD_REQUEST)
+      return Response.status(BAD_REQUEST.getStatusCode())
         .type(MediaType.TEXT_PLAIN)
         .entity(throwable.getMessage())
         .build();
     }
     if (throwable instanceof NotFoundException) {
-      return Response.status(HttpStatus.SC_NOT_FOUND)
+      return Response.status(NOT_FOUND.getStatusCode())
         .type(MediaType.TEXT_PLAIN)
         .entity(throwable.getMessage())
         .build();
     }
     if (throwable instanceof ConflictException) {
-      return Response.status(HttpStatus.SC_CONFLICT)
+      return Response.status(CONFLICT.getStatusCode())
         .type(MediaType.TEXT_PLAIN)
         .entity(throwable.getMessage())
         .build();
@@ -42,15 +46,15 @@ public final class ExceptionHelper {
     ValidationHelper.handleError(throwable, validationFuture.completer());
     if (validationFuture.isComplete()) {
       Response response = validationFuture.result();
-      if (response.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
+      if (response.getStatus() == INTERNAL_SERVER_ERROR.getStatusCode()) {
         LOGGER.error(throwable.getMessage(), throwable);
       }
       return response;
     }
     LOGGER.error(throwable.getMessage(), throwable);
-    return Response.status(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+    return Response.status(INTERNAL_SERVER_ERROR.getStatusCode())
       .type(MediaType.TEXT_PLAIN)
-      .entity(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase())
+      .entity(INTERNAL_SERVER_ERROR.getReasonPhrase())
       .build();
   }
 }
